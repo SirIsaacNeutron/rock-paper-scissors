0e293a8e5cf92789ec83f4c697d963d411e68c54
SirIsaacNeutron <dbadir@cox.net>
2022-08-23 01:00:16 -0700

    Add score counter
    The game will play for five rounds, but does NOT add extra rounds to
    deal with draws. This means that a game can actually end in a score of
    0-0 for a draw.

--- game.js
+++ game.js
@@ -55,23 +55,51 @@ function playRound(playerSelection, computerSelection) {
     else {
         resultString = getResultString(isPlayerWinner, playerSelection, 'You lose!')
     }
-    return resultString
+    return [isPlayerWinner, resultString]
+}
+
+function getPlayerSelection() {
+    let playerSelection = prompt('Please enter your choice for Rock Paper Scissors:')
+
+    while (!OPTIONS.includes(playerSelection.toLowerCase())) {
+        playerSelection = prompt(`Error: ${playerSelection} is not a valid option. Please select 'rock,' 'paper,' or 'scissors.'`)
+    }
+
+    return playerSelection.toLowerCase()
 }
 
 function game() {
+    let playerScore = 0
+    let computerScore = 0
     for (let i = 0; i < 5; ++i) {
-        let playerSelection = prompt('Please enter your choice for Rock Paper Scissors:')
-        while (playerSelection === null) {
-            playerSelection = prompt('Error: no answer detected. Please enter your choice for Rock Paper Scissors:')
-        }
+        let playerSelection = getPlayerSelection()
         let computerSelection = getComputerChoice()
-        playerSelection = playerSelection.toLowerCase()
 
         if (playerSelection === computerSelection) {
             console.log('Tie! You have both chosen ' + playerSelection + '!')
         }
         else {
-            console.log(playRound(playerSelection, computerSelection))
+            const [isPlayerWinner, resultString] = playRound(playerSelection, computerSelection)
+            if (isPlayerWinner) {
+                ++playerScore
+            }
+            else {
+
+                ++computerScore
+            }
+            console.log(resultString)
         }
+        console.log(`Total score: Player ${playerScore}; Computer ${computerScore}`)
+        console.log()
+    }
+
+    if (playerScore > computerScore) {
+        console.log('You win!')
+    }
+    else if (computerScore > playerScore) {
+        console.log('You lose!')
+    }
+    else {
+        console.log("It's a draw!")
     }
 }
